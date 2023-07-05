66ad144a0cbcb57d924c801c9492f899b0ce87b2
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-07-05 17:31:09 +0500

    update

--- library/library/settings.py
+++ library/library/settings.py
@@ -56,7 +56,10 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500/',
 ]
-
+CORS_ORIGINS_WHITELIST = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000'
+]
 ROOT_URLCONF = 'library.urls'
 
 TEMPLATES = [
